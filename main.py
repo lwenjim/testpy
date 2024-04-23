c53e4d3dbfef0ebb51f9370829dc335250de2329
liuwenjin <liuwenjin@jspp.com>
2024-04-23 21:21:46 +0800

    feat: reset file

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
-def quickSort(arr, left=None, right=None):
-    left = 0 if not isinstance(left,(int, float)) else left
-    right = len(arr)-1 if not isinstance(right,(int, float)) else right
-    if left < right:
-        partitionIndex = partition(arr, left, right)
-        quickSort(arr, left, partitionIndex-1)
-        quickSort(arr, partitionIndex+1, right)
-    return arr
+a = None
 
-def partition(arr, left, right):
-    pivot = left
-    index = pivot+1
-    i = index
-    while  i <= right:
-        if arr[i] < arr[pivot]:
-            swap(arr, i, index)
-            index+=1
-        i+=1
-    swap(arr,pivot,index-1)
-    return index-1
 
-def swap(arr, i, j):
-    arr[i], arr[j] = arr[j], arr[i]
+def main():
+    global a
+    if a is None:
+        a = 1
 
-arr = [3, 2, 4, 1]
-quickSort(arr)
-for i in arr:
-    print(i);
+
+main()
+
+print(a)
